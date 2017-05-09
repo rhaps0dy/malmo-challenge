795c7b99ba5a17a9828dc679b13ea7ad92659daf
Adrià Garriga <adria.garriga@gmail.com>
2017-05-09 18:09:54 +0100

    More advances

--- MCTS/main.cpp
+++ MCTS/main.cpp
@@ -2,9 +2,14 @@
 #include <cmath>
 #include <cassert>
 #include <iostream>
+#include <utility>
+
+// Learn the strategy of the pig
+// Learn the possible strategy/s of the other agent
 
 using namespace std;
 
+typedef float Float;
 const bool WALLS[7][7] = {
 	{0,0,0,0,0,0,0},
 	{0,1,1,1,1,1,0},
@@ -35,9 +40,27 @@ struct Player {
 	Direction d;
 };
 
+struct Pig {
+	int x, y;
+};
+
+class Strategy {
+	virtual Action act(Player p1, Player p2, int t, Pig pig) = 0;
+};
+
+class Left : Strategy {
+	virtual Action act(Player p1, Player p2, int t, Pig pig) { return A_LEFT; }
+};
+class Right : Strategy {
+	virtual Action act(Player p1, Player p2, int t, Pig pig) { return A_RIGHT; }
+};
+class Front : Strategy {
+	virtual Action act(Player p1, Player p2, int t, Pig pig) { return A_FRONT; }
+};
+
 struct Node {
 	const Node &parent;
-	float value;
+	Float value_sum;
 	int n_visits;
 	bool is_final;
 	Action prev_action;
@@ -45,8 +68,9 @@ struct Node {
 
 	// State of the world
 	int t;
+	Pig pig
 	Player ps[2];
-	static int P_x, P_y; // pig
+	static vector<pair<Float, Strategy> > p1_strats;
 
 protected:
 	vector<Node> children;
@@ -54,20 +78,22 @@ protected:
 public:
 
 	Node(const Node &_parent, int x, int y, Direction d, Action _prev_a) :
-		parent(_parent), value(NAN), n_visits(0), is_final(false),
-		prev_action(_prev_a), visited_all_children(false), t(parent.t+1) {
+		parent(_parent), value_sum(0.0), n_visits(0), is_final(false),
+		prev_action(_prev_a), visited_all_children(false), t(parent.t+1), pig(parent.pig) {
 		const int role=t%2;
 		if(role==1 && ((x == 0) && (y == 3) || (x == 6) && (y == 3) || t >= MAX_T)) {
 			is_final = true;
 		}
 	}
 
+	// Constructor for the root
 	Node(int x0, int y0, Direction d0, int x1, int y1, Direction d1, int _P_x, int _P_y) :
-		parent(*this), value(NAN), n_visits(0), is_final(false),
+		parent(*this), value_sum(0.0), n_visits(0), is_final(false),
 		prev_action(A_FRONT), visited_all_children(false), t(0) {
 		ps[0].x = x0; ps[0].y = y0; ps[0].d = d0;
 		ps[1].x = x1; ps[1].y = y1; ps[1].d = d1;
-		P_x = _P_x; P_y = _P_y;
+		pig.x = _P_x; pig.y = _P_y;
+
 	}
 
 	const vector<Node> &get_children() {
@@ -101,6 +127,17 @@ public:
 		}
 		return children;
 	}
+
+	const Float value(const Float c) {
+		if(is_final) {
+			Float v = -(t/2);
+			if(ps[0].x == 
+			return v;
+		}
+		return value_sum/n_visits + c*sqrt(2.*log(parent.n_visits)/n_visits);
+	}
+
+	Node &simulate_path(
 };
 
 
