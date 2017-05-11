a08b02e4d3ce553c996607422250d2b002964832
Adrià Garriga <adria.garriga@gmail.com>
2017-05-11 20:33:54 +0100

    Added goals and heuristic

--- MCTS/astar.hpp
+++ MCTS/astar.hpp
@@ -1,6 +1,10 @@
 #pragma once
+#include <cstdlib>
 
 #include "node.hpp"
+
+typedef int (*AStarHeuristic)(const Node &);
+typedef bool (*AStarGoal)(const Node &);
 
 class AStarSearch {
 public:
@@ -8,20 +12,18 @@ public:
 		int _P_x, int _P_y);
 };
 
-struct AStartNodeCompare {
+struct AStarNodeCompare {
 	bool operator() (const AStarNode& lhs, const AStarNode& rhs) const {
 		return lhs.get_serialization() < rhs.get_serialization();
 	}
-}
-
-class AStarNode : public Node<AStarNode> {
-protected:
-	int g_cost; // accumulated cost
-
-public:
-	AStarNode(const AStarNode *_parent, int x, int y, Direction d, Action _prev_a);
-	AStarNode(int x0, int y0, Direction d0, int x1, int y1, Direction d1,
-			int _P_x, int _P_y);
-	int get_heuristic_cost() const;
-	int get_f_cost() const;
 };
+
+bool exit_goal(const Node &n) {
+	return n.in_exit(0);
+}
+int exit_heuristic(const Node &n) {
+	// Manhattan distance of p0 to closest exit
+	const int y_dist = abs(n.ps[0].y - 3);
+	const int x_dist = min(abs(n.ps[0].x - 1), abs(n.ps[0].x - 7));
+	return (y_dist + x_dist) * 2;
+}
