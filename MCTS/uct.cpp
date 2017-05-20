50e77b1e87d776c022f8f1f39dc6f26aebee01de
Adrià Garriga <adria.garriga@gmail.com>
2017-05-20 11:39:23 +0100

    commented UCT code, added support for tree/default policy

--- MCTS/uct.cpp
+++ MCTS/uct.cpp
@@ -16,61 +16,107 @@ static unordered_map<NodeSeri, array<int, N_ACTIONS+1> > _n_visits;
 
 static unordered_map<NodeSeri, array<Float, N_ACTIONS> > _value_sum;
 
-static
-Action best_child_action(std::array<int, N_ACTIONS+1> &n_visits,
-						 array<Float, N_ACTIONS> &value_sum,
-						 const Float constant) {
+// Returns the best action to take from the node given by n_visits and
+// value_sum, or the node without any visits if it exists. This is equivalent to
+// taking one step in the UCT tree policy.
+//
+// The paremeter `no_action` is assigned value `true` if the action returned has
+// never been taken from the given node.
+static Action
+tree_policy_action(std::array<int, N_ACTIONS+1> &n_visits,
+				   array<Float, N_ACTIONS> &value_sum, const Float constant,
+				   bool &no_action) {
 	size_t n_best=0;
 	Action best[N_ACTIONS];
 	Float max_val = -INFINITY;
 
 	for(size_t i=0; i<N_ACTIONS; i++) {
 		if(n_visits[i] == 0) {
-			if(!isnan(max_val))
+			// Is there an action that has never been taken?
+			if(!isnan(max_val)) {
+				// If so, and this is the first action we see that hasn't been
+				// taken, discard other actions to randomly choose
 				n_best = 0;
+			}
 			max_val = NAN;
 			best[n_best++] = static_cast<Action>(i);
 		} else if(!isnan(max_val)) {
+			// Otherwise, if no other action has never been taken,
+			// calculate the UCT value
 			Float val = value_sum[i]/n_visits[i] +
 				constant*sqrt(2.*log(n_visits[N_ACTIONS])/n_visits[i]);
+
+			// And if it's the highest, add this node to the list of equally
+			// best nodes.
 			if(val > max_val) {
 				max_val = val;
 				n_best = 1;
 				best[0] = static_cast<Action>(i);
 			} else if (val == max_val) {
+				// Ties are broken uniformly randomly
 				best[n_best++] = static_cast<Action>(i);
 			}
 		}
 	}
 	assert(n_best > 0);
+	// Break ties uniformly randomly
 	return best[Random::uniform_int<size_t>(n_best-1)];
 }
 
 #define TIME_SERIALIZATION(node) (node.get_serialization()*(MAX_T+1) + node.t)
 static
 void simulate_path(Node current, Float constant, StrategyChooser &sc) {
+	// contains pointers to the <node, action> 's sum of values that we have
+	// passed through to get to the final node.
 	static vector<Float*> values;
 	values.clear();
 
+	// A strategy (aka a policy) that the other agent takes, drawn from its
+	// strategy mix
 	Strategy &strategy = sc.random_strat();
 	strategy.reset();
 
+	// Whether our agent takes the tree policy or the default policy.
+	bool using_default_policy = false;
+
 	while(!current.is_final) {
 		if(current.t%2 == 0) {
+			// Get key of this node in the _n_visits and _value_sum maps. Then
+			// fetch (or create if they don't exist) this node's arrays in them.
 			const NodeSeri ns = TIME_SERIALIZATION(current);
-			auto &n_visits = _n_visits.insert({ns, {{0}}}).first->second;
-			auto &value_sum = _value_sum.insert({ns, {{0.0}}}).first->second;
-			const Action best_a = best_child_action(n_visits, value_sum,
-													constant);
+			auto &n_visits = _n_visits.insert({ns, {{0,0,0,0}}}).first->second;
+			auto &value_sum = _value_sum.insert({ns, {{0.0,0.0,0.0}}}).first->second;
+
+			// Choose the immediate child according to the tree policy or the default
+			// policy. Note that we don't choose the end result of the tree or
+			// default policy, only the child of this node according to that
+			// policy.
+			// This also modifies the `using_default_policy` variable, to set it
+			// to `true` if the tree policy should finish.
+			const Action best_a =
+				(using_default_policy ?
+				 /* default_policy_action(...) */ A_RIGHT :
+				 tree_policy_action(n_visits, value_sum, constant,
+									using_default_policy));
+
+			// Record this ancestor's <node, value> pair for later backup
 			values.push_back(&value_sum[best_a]);
+
+			// Add 1 to the number of visits of <node,action> pair. This is
+			// usually done when backing up, but it's faster to do it here.
 			n_visits[best_a]++;
 			n_visits[N_ACTIONS]++;
+
 			current.make_child(best_a, false);
 		} else {
+			// Turn for the other agent, it takes an action according to its
+			// policy
 			const Action a = strategy.act(current);
 			current.make_child(a, true);
 		}
 	}
+	// Backup rule: value is the reward obtained in the leaf. Then we go up
+	// through this leaf's ancestors and we add the value to them.
 	const Float value = static_cast<Float>(current.value + 25) / 50.;
 	for(auto v: values)
 		(*v) += value;
@@ -155,8 +201,8 @@ Action uct_best_action(Node &root, int budget,
 	const NodeSeri root_seri = TIME_SERIALIZATION(root);
 	auto a = _n_visits.find(root_seri);
 	auto b = _value_sum.find(root_seri);
-	return best_child_action(a->second,
-							 b->second, 0.0);
+	bool _dummy;
+	return tree_policy_action(a->second, b->second, 0.0, _dummy);
 }
 #undef TIME_SERIALIZATION
 
